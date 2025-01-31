ff774b920171c8b3545dafe42bc6c400d791852a
William Tavares <williamtavarescd@gmail.com>
2025-01-31 09:28:22 -0300

    duque

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  app: {
+    head: {
+      title: "DD - Duque Dev",
+      meta: [
+        { name: "description", content: "projeto kraken do dev duque" }
+      ]
+    }
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   vite: {
